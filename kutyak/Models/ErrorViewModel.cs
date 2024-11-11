a3022937c00567b2a1bbd451a44af661e09a6919
ZZalan001 <zima.zalan1@gmail.com>
2024-11-11 11:54:02 +0100

    Add project files.

---
+++ kutyak/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace kutyak.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
